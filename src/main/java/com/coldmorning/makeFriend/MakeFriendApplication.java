af7c63aca60c43fb2114c345b6fdfdcd54b0d295
coldmorning <yulianghuang360@gmail.com>
2021-07-11 09:17:50 +0800

    feat(All) : Create the project.

---
+++ src/main/java/com/coldmorning/makeFriend/MakeFriendApplication.java
@@ -0,0 +1,13 @@
+package com.coldmorning.makeFriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeFriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coldmorning/makeFriend/MakeFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coldmorning.makeFriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
